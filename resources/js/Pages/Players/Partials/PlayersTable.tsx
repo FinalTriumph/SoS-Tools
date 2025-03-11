eaf47abe34359d15f1f937277c12a1ded63dd5bb
FinalTriumph <kaspars_andzans@inbox.lv>
2025-03-11 16:02:09 +0200

    option to download players table as image

--- resources/js/Pages/Players/Partials/PlayersTable.tsx
+++ resources/js/Pages/Players/Partials/PlayersTable.tsx
@@ -1,9 +1,13 @@
+import PrimaryButton from '@/Components/PrimaryButton';
 import { Player } from '@/types';
 import { useForm } from '@inertiajs/react';
+import { useRef } from 'react';
+import html2canvas from 'html2canvas';
 import PlayerRow from './PlayerRow';
 
 export default function PlayersTable({ players }: { players: Player[] }) {
     const { delete: destroy } = useForm();
+    const tableRef = useRef<HTMLTableElement>(null);
 
     const handleDelete = (id: number, username: string) => {
         if (confirm(`Are you sure you want to delete ${username}?`)) {
@@ -14,39 +18,62 @@ export default function PlayersTable({ players }: { players: Player[] }) {
         }
     };
 
+    const handleDownload = async () => {
+        if (!tableRef.current) return;
+    
+        const canvas = await html2canvas(tableRef.current, {
+            scale: 2,
+        });
+    
+        const dataURL = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = dataURL;
+        link.download = `st_players.png`;
+        link.click();
+    };
+
     return (
-        <table className="w-full table-auto mt-6">
-            <thead className="bg-gray-100">
-                <tr>
-                    <th rowSpan={2} />
-                    <th rowSpan={2} colSpan={2}>Player</th>
-                    <th colSpan={3}>Behemoths</th>
-                    <th colSpan={3}>Squadron</th>
-                    <th rowSpan={2}>Behemoths BP<br />+<br />Squadron BP</th>
-                    <th rowSpan={2}>Troops</th>
-                    <th rowSpan={2}>Player</th>
-                    <th rowSpan={2} />
-                    <th rowSpan={2}/>
-                </tr>
-                <tr>
-                    <th>BP</th>
-                    <th>MK1</th>
-                    <th>MK2</th>
-                    <th>BP</th>
-                    <th>Formation System</th>
-                    <th>FA-1 Specter</th>
-                </tr>
-            </thead>
-            <tbody>
-                {players.map((player, index) => (
-                    <PlayerRow
-                        key={player.id}
-                        player={player}
-                        rank={index + 1}
-                        onDelete={() => handleDelete(player.id, player.username)}
-                    />
-                ))}
-            </tbody>
-        </table>
+        <div>
+            <table ref={tableRef} className="w-full table-auto mt-6">
+                <thead className="bg-gray-100">
+                    <tr>
+                        <th rowSpan={2} />
+                        <th rowSpan={2} colSpan={2}>Player</th>
+                        <th colSpan={3}>Behemoths</th>
+                        <th colSpan={3}>Squadron</th>
+                        <th rowSpan={2}>Behemoths BP<br />+<br />Squadron BP</th>
+                        <th rowSpan={2}>Troops</th>
+                        <th rowSpan={2}>Player</th>
+                        <th rowSpan={2} />
+                        <th rowSpan={2}/>
+                    </tr>
+                    <tr>
+                        <th>BP</th>
+                        <th>MK1</th>
+                        <th>MK2</th>
+                        <th>BP</th>
+                        <th>Formation System</th>
+                        <th>FA-1 Specter</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.map((player, index) => (
+                        <PlayerRow
+                            key={player.id}
+                            player={player}
+                            rank={index + 1}
+                            onDelete={() => handleDelete(player.id, player.username)}
+                        />
+                    ))}
+                </tbody>
+            </table>
+
+            <PrimaryButton
+                className="mt-6"
+                onClick={() => handleDownload()}
+            >
+                Download
+            </PrimaryButton>
+        </div>
     );
 }
