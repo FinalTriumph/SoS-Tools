a5e1b82fbd222c97c021dd3463b1923ffcc9f0ed
FinalTriumph <kaspars_andzans@inbox.lv>
2025-03-17 14:22:08 +0200

    add back vite prefetch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
+        Vite::prefetch(concurrency: 3);
+
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
